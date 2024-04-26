dfbb148981dea7cbe0c5dff2aea57b47000e018b
minhaz74692 <minhaz74692@gmail.com>
2024-04-26 12:03:51 +0530

    data_validation of sales_data

---
+++ sql_files/calculation_in_SQL/datq_validation.sql
@@ -0,0 +1,18 @@
+with sales_history as (
+  SELECT
+    EXTRACT(YEAR FROM Date) AS YEAR --time grouping
+  , EXTRACT(MONTH FROM Date) AS MONTH --time grouping
+  , ProductId --need to know which products are sold
+  , StoreID --need to know which stores are selling
+  , SUM(quantity) AS UnitsSold --how many (impacts inventory)
+  , AVG(UnitPrice) AS UnitPriceProxy --can be interesting
+  , COUNT(DISTINCT salesID) AS NumTransactions --unique transactions can be interesting
+  FROM `data-anlytics-419117.imported_datasets.sales` 
+  GROUP BY   YEAR,   MONTH, ProductId, StoreID
+)
+SELECT
+ inventory.*
+ , (SELECT AVG(UnitsSold) FROM sales_history
+      WHERE inventory.ProductID=sales_history.ProductID
+      AND inventory.StoreID=sales_history.StoreID) AS avg_quantity_sold_in_a_month
+FROM `data-anlytics-419117.imported_datasets.inventory`  AS inventory
